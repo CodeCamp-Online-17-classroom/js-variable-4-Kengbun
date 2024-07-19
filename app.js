5bf3cafb208f57a3bfc859d6024ed736b220e8b6
keng <285keng+github@gmail.com>
2024-07-19 14:17:24 +0700

    lab4 ประกาศชื่อตัวแปร and add name

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-// write code here
+const firstName ="Nantachai";
+let lasName = "Namwong";
+var nickname = "keng";
+
+
+// เพื่อน
+const firstName ="sang";
+let lasName = "jun";
+var nickname = "bun";
+
+birthDate ="10-10-2021";
